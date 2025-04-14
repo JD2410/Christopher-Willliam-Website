a0f50f7655229c9efa1bac569dfe71f54444dccb
Jonathan Delves <jonathandelves@hotmail.com>
2025-04-14 08:36:58 +0100

    thumbnail click to open gallery

--- jscript/gallery.js
+++ jscript/gallery.js
@@ -1,9 +1,41 @@
-scr = {
+function Gallery(title, description, pageThumbnail, images) {
+    this.title = title;
+    this.description = description;
+    this.thumbnail = pageThumbnail;
+    this.images = images;
+}
+
+let galleries = [
+    new Gallery(null, null, 'images/proj-01.jpg', [
+        'gallery/gallery-01/gallery-01.jpg',
+        'gallery/gallery-01/gallery-02.jpg',
+        'gallery/gallery-01/gallery-03.jpg',
+    ]),
+    new Gallery('Title', null, 'images/proj-02.jpg',[
+        'gallery/gallery-01/gallery-02.jpg',
+        'gallery/gallery-01/gallery-01.jpg',
+        'gallery/gallery-01/gallery-03.jpg',
+    ]),
+    new Gallery(null, 'lorem ipsum donor', 'images/proj-03.jpg', [
+        'gallery/gallery-01/gallery-03.jpg',
+        'gallery/gallery-01/gallery-02.jpg',
+        'gallery/gallery-01/gallery-01.jpg',
+    ])
+];
+
+
+let scr = {
     init: function() {
         document.addEventListener('keydown', function(event) {
             if(event.key === 'Escape') {
                 scr.hideGallery();
             }
+        })
+        let galleryThumbnails = document.getElementsByClassName("thumbnails")
+        Array.from(galleryThumbnails).forEach(function(element) {
+            element.addEventListener('click', function() {
+                scr.showGallery();
+            })
         })
     },
     showGallery: function() {
@@ -39,5 +71,5 @@ scr = {
 
 window.onload = (Event) => {
     scr.init();
-    scr.showGallery()
+   //scr.showGallery()
 }
