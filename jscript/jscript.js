792c63d7f836cfc46cebd65aaacf9c376a09883b
Jonathan Delves <jonathandelves@hotmail.com>
2025-04-16 14:21:40 +0100

    Contact form styling and enhanced inputs

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -5,6 +5,7 @@ let cwbs = {
         })
         this.formScript();
         this.inputs();
+        this.textarea();
     },
     formScript: function() {
 
@@ -46,46 +47,73 @@ let cwbs = {
                   });
           });
     },
+    textarea: function() {
+        let textareas = document.querySelectorAll(".cus-textarea")
+        for(let textarea of textareas) {
+
+            const $textarea = textarea.getElementsByTagName('textarea')[0];
+
+            textarea.addEventListener("click", function() {
+                let $this = this;
+                $this.classList.add("selected");
+                $this.classList.add("selected-colour");
+                $textarea.select();
+            })
+
+            $textarea.addEventListener("focus", function() {
+                let $this = this;
+                let $outer = $this.closest(".cus-textarea")
+                $outer.classList.add("selected");
+                $outer.classList.add("selected-colour");
+                $textarea.select();
+            })
+
+            $textarea.addEventListener("blur", function() {
+                let $this = this;
+                textarea.classList.remove("selected-colour")
+                if($this.value == "") {
+                    textarea.classList.remove("selected")
+                }
+            })
+        }
+    },
     inputs: function() {
-        let inputs = document.querySelectorAll(".uhr-input")
+        let inputs = document.querySelectorAll(".cus-input")
         for(let input of inputs) {
 
-            if(!input.classList.contains('nf')) {
+            const $input = input.getElementsByTagName('input')[0];
+           
+            input.addEventListener("click", function() {
+                let $this = this;
+                $this.classList.add("selected");
+                $this.classList.add("selected-colour");
+                $input.select();
+            })
 
-                const $input = input.getElementsByTagName('input')[0];
-                input.addEventListener("click", function() {
-                    let $this = this;
-                    $this.classList.add("selected");
-                    $this.classList.add("selected-colour");
-                    $input.select();
-                })
+            $input.addEventListener("focus", function() {
+                let $this = this;
+                let $outer = $this.closest(".cus-input")
+                $outer.classList.add("selected");
+                $outer.classList.add("selected-colour");
+                $input.select();
+            })
 
-                $input.addEventListener("focus", function() {
-                    let $this = this;
-                    let $outer = $this.closest(".uhr-input")
-                    $outer.classList.add("selected");
-                    $outer.classList.add("selected-colour");
-                    $input.select();
-                })
+            $input.addEventListener("blur", function() {
+                let $this = this;
+                input.classList.remove("selected-colour")
+                if($this.value == "") {
+                    input.classList.remove("selected")
+                }
+            })
 
-                $input.addEventListener("blur", function() {
-                    let $this = this;
-                    input.classList.remove("selected-colour")
-                    if($this.value == "") {
-                        input.classList.remove("selected")
-                    }
-                })
-
-                $input.addEventListener("keyup", function() {
-                    let $this = this;
-                    if($this.value.toUpperCase() == "ERROR") {
-                        input.classList.add("error")
-                    } else {
-                        input.classList.remove("error")
-                    }
-                })
-            }
-            
+            $input.addEventListener("keyup", function() {
+                let $this = this;
+                if($this.value.toUpperCase() == "ERROR") {
+                    input.classList.add("error")
+                } else {
+                    input.classList.remove("error")
+                }
+            })
         }
     },
 }
