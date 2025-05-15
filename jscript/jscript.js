6a1b0265303836034b6f34ab82c1b7e184dcf6f3
Jonathan Delves <jonathandelves@hotmail.com>
2025-05-15 15:01:19 +0100

    Contact Form Messages

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -11,6 +11,11 @@ let cwbs = {
                 document.getElementById("navigation").classList.toggle('open')
             })
         })
+
+        document.getElementById('result-container').addEventListener('click', function() {
+            this.removeAttribute("style");
+        })
+
         this.formScript();
         this.formInputStyling();
         cwbs.cookie.init();
@@ -42,7 +47,7 @@ let cwbs = {
     formScript: function() {
 
         const form = document.getElementById('form');
-        const result = document.getElementById('result');
+        
         form.addEventListener('submit', function(e) {
             e.preventDefault();
 
@@ -55,8 +60,8 @@ let cwbs = {
                     const formData = new FormData(form);
                     const object = Object.fromEntries(formData);
                     const json = JSON.stringify(object);
-                    result.innerHTML = "Please wait..."
-                
+                    cwbs.contectFormMessage("Please wait...", "info")
+
                     fetch('https://api.web3forms.com/submit', {
                             method: 'POST',
                             headers: {
@@ -68,31 +73,39 @@ let cwbs = {
                         .then(async (response) => {
                             let json = await response.json();
                             if (response.status == 200) {
-                                result.innerHTML = "Form submitted successfully";
+                                cwbs.contectFormMessage("Form submitted successfully", "success")
                             } else {
                                 console.log(response);
-                                result.innerHTML = json.message;
+                                cwbs.contectFormMessage(json.message, "failed")
                             }
                         })
                         .catch(error => {
                             console.log(error);
-                            result.innerHTML = "Something went wrong!";
+                            cwbs.contectFormMessage("Something went wrong!", "failed")
                         })
                         .then(function() {
                             form.reset();
-                            setTimeout(() => {
-                                result.style.display = "none";
-                            }, 3000);
                         });
                     }
                 } else {
-                    result.innerHTML = "Please confirm you have understood our usage of cookies in the popup";
-                    setTimeout(() => {
-                        result.style.display = "none";
-                    }, 3000);
+                    cwbs.contectFormMessage("Please confirm you have understood our usage of cookies in the popup", "info")
                 }
             
           });
+    },
+    contectFormMessage: function(message, status) {
+        
+        const resultMessage = document.getElementById('result-message');
+        const resultCon = document.getElementById('result-container');
+
+        resultCon.classList.remove("success")
+        resultCon.classList.remove("info")
+        resultCon.classList.remove("failed")
+
+        resultMessage.innerHTML = message;
+        resultCon.classList.add(status);
+        resultCon.style.display = "flex";
+
     },
     formInputStyling: function() {
         let inputs = document.querySelectorAll(".cus-input")
