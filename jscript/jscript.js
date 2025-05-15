4489a0da9d37c9100da100bae16f7e8509d8bb83
Jonathan Delves <jonathandelves@hotmail.com>
2025-05-15 13:55:03 +0100

    Red to Gradient upgrade - result message for popup confirmation script

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -45,45 +45,53 @@ let cwbs = {
         const result = document.getElementById('result');
         form.addEventListener('submit', function(e) {
             e.preventDefault();
-            const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
-            
-            if (!hCaptcha) {
-                e.preventDefault();
-                alert("Please fill out captcha field")
-            } else {
-            const formData = new FormData(form);
-            const object = Object.fromEntries(formData);
-            const json = JSON.stringify(object);
-            result.innerHTML = "Please wait..."
-          
-              fetch('https://api.web3forms.com/submit', {
-                      method: 'POST',
-                      headers: {
-                          'Content-Type': 'application/json',
-                          'Accept': 'application/json'
-                      },
-                      body: json
-                  })
-                  .then(async (response) => {
-                      let json = await response.json();
-                      if (response.status == 200) {
-                          result.innerHTML = "Form submitted successfully";
-                      } else {
-                          console.log(response);
-                          result.innerHTML = json.message;
-                      }
-                  })
-                  .catch(error => {
-                      console.log(error);
-                      result.innerHTML = "Something went wrong!";
-                  })
-                  .then(function() {
-                      form.reset();
-                      setTimeout(() => {
-                          result.style.display = "none";
-                      }, 3000);
-                  });
+
+            if (document.cookie.includes("cookie-consent=1")) { 
+                const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
+                if (!hCaptcha) {
+                    e.preventDefault();
+                    alert("Please fill out captcha field")
+                } else {
+                    const formData = new FormData(form);
+                    const object = Object.fromEntries(formData);
+                    const json = JSON.stringify(object);
+                    result.innerHTML = "Please wait..."
+                
+                    fetch('https://api.web3forms.com/submit', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json'
+                            },
+                            body: json
+                        })
+                        .then(async (response) => {
+                            let json = await response.json();
+                            if (response.status == 200) {
+                                result.innerHTML = "Form submitted successfully";
+                            } else {
+                                console.log(response);
+                                result.innerHTML = json.message;
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            result.innerHTML = "Something went wrong!";
+                        })
+                        .then(function() {
+                            form.reset();
+                            setTimeout(() => {
+                                result.style.display = "none";
+                            }, 3000);
+                        });
+                    }
+                } else {
+                    result.innerHTML = "Please confirm you have understood our usage of cookies in the popup";
+                    setTimeout(() => {
+                        result.style.display = "none";
+                    }, 3000);
                 }
+            
           });
     },
     formInputStyling: function() {
