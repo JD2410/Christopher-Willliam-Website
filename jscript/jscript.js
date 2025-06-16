58b2e167fd0d90882b23c9f8bfdad73b520a2e01
Jonathan Delves <jonathandelves@hotmail.com>
2025-06-16 18:03:00 +0100

    Inital script in place

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -25,8 +25,9 @@ let cwbs = {
     heroImage: {
         primaryImages: ["big-hero.png", "big-hero-2.png", "big-hero-3.png"],
         secondaryImages: ["small-hero.png", "small-hero-2.png", "small-hero-3.png"],
-        selection: 0,
+        selection: 1,
         path: "images/hero-images/",
+        state: true,
         init: () => {
             const primaryContainer = document.getElementById("primary-image");
             const secondaryContainer = document.getElementById("secondary-image");
@@ -38,7 +39,36 @@ let cwbs = {
             let scondaryBacking = document.createElement("img");
             scondaryBacking.src = cwbs.heroImage.path + cwbs.heroImage.secondaryImages[1];
             secondaryContainer.appendChild(scondaryBacking);
+        },
+        loadNextImage: () => {
 
+            cwbs.heroImage.selection++;
+
+            if(cwbs.heroImage.selection > (cwbs.heroImage.primaryImages.length - 1)) {
+                cwbs.heroImage.selection = 0;
+            }
+            if (cwbs.heroImage.state) {
+                document.getElementById("primary-image").classList.add("fade")
+                document.getElementById("secondary-image").classList.add("fade");
+
+                setTimeout(() => {
+                    document.querySelector("#primary-image img:first-child").src = cwbs.heroImage.path + cwbs.heroImage.primaryImages[cwbs.heroImage.selection];
+                    document.querySelector("#secondary-image img:first-child").src = cwbs.heroImage.path + cwbs.heroImage.secondaryImages[cwbs.heroImage.selection];
+                }, 3000)
+
+                cwbs.heroImage.state = false;
+            } else {
+                document.getElementById("primary-image").classList.remove("fade")
+                document.getElementById("secondary-image").classList.remove("fade");
+
+                setTimeout(() => {
+                    document.querySelector("#primary-image img:nth-child(2)").src = cwbs.heroImage.path + cwbs.heroImage.primaryImages[cwbs.heroImage.selection];
+                    document.querySelector("#secondary-image img:nth-child(2)").src = cwbs.heroImage.path + cwbs.heroImage.secondaryImages[cwbs.heroImage.selection];
+                }, 3000)
+
+                cwbs.heroImage.state = true;
+            }
+            
         }
     },
     cookie: {
